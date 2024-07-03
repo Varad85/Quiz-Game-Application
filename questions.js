da9e01418981d5b9833591aaacebd41777385c69
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-03 17:15:28 +0530

    added question objects for Brazil,New Zealand,Greece,Iran and India

---
+++ questions.js
@@ -0,0 +1,41 @@
+let MCQS=[{
+	question:"Capital of Brazil is",
+	option1:"Brussels",
+	option2:"Brasilia",
+	option3:"Belmopan",
+	option4:"Sofia",
+	answer:"Brasilia"
+},
+{
+    question:"Capital of New Zealand is",
+	option1:"Ngerulmud",
+	option2:"Funafati",
+	option3:"Wellington",
+	option4:"Apia",
+	answer:"Wellington"
+},
+{
+	question:"Capital of Greece is",
+	option1:"Berlin",
+	option2:"Athens",
+	option3:"Port au Prince",
+	option4:"Bissau",
+	answer:"Athens"
+},
+{
+	question:"Capital of Iran is",
+	option1:"Tehran",
+	option2:"Budapest",
+	option3:"Accra",
+	option4:"Jakarta",
+	answer:"Tehran"
+},
+{
+	question:"Capital of India is",
+	option1:"Chennai",
+	option2:"Mumbai",
+	option3:"Kolkata",
+	option4:"New Delhi",
+	answer:"New Delhi"
+},]
+
